d8d8f2929fe617d15d943a81986758e58004833b
E. R. Nilsson <e.rasmus.nilsson@gmail.com>
2023-12-02 12:51:06 +0100

    Adds more variable set tests

--- exchange/config_test.go
+++ exchange/config_test.go
@@ -76,7 +76,7 @@ func Test_VariableSet_SubstituteLine_GivenNonExistentKeyWithoutDefault_ReturnsEr
 	}
 }
 
-func Test_VariableSet_SubstituteLine_MalformedKey_ReturnsError(t *testing.T) {
+func Test_VariableSet_SubstituteLine_GivenMalformedKey_ReturnsError(t *testing.T) {
 	set := make(VariableSet)
 	line := "this is a ${var.type:default:another_default} with var substitution"
 	_, err := set.SubstituteLine(line)
@@ -84,5 +84,21 @@ func Test_VariableSet_SubstituteLine_MalformedKey_ReturnsError(t *testing.T) {
 	if err == nil {
 		t.Errorf("expected error to be returned but got nil")
 		return
+	}
+}
+
+func Test_VariableSet_SubstituteLine_GivenSubstitutionWithAndWithoutDefault_Substitutes(t *testing.T) {
+	set := make(VariableSet)
+	set["sub"] = "substitution"
+	line := "this is a ${var.type:line} with var ${var.sub}"
+	processed, err := set.SubstituteLine(line)
+
+	if err != nil {
+		t.Errorf("received unexpected error: %s", err)
+		return
+	}
+	expected := "this is a line with var substitution"
+	if processed != expected {
+		t.Errorf("expected line '%s' but got: %s", expected, processed)
 	}
 }
